3ec45d772178e883ffc28b7b1176f7a7f3e7a6bc
Clément Hamada <clementhamada@protonmail.com>
2020-04-26 17:16:12 +0200

    ex04

---
+++ ex04/ex04.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+int main(void) {
+	std::string brain = "HI THIS IS BRAIN";
+
+	std::string *brain_ptr = &brain;
+	std::string &brain_ref = brain;
+
+	std::cout << *brain_ptr << std::endl;
+	std::cout << brain_ref << std::endl;
+
+	return (0);
+}
