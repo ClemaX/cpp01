28c4d34053c98c97da8d98389dd176574058c33e
Clément Hamada <clementhamada@protonmail.com>
2021-01-16 16:52:31 +0100

    ex01: Add leaks.sh

---
+++ ex01/leaks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME="a.out"
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+MEMCHECK=""
+
+find_memcheck()
+{
+	if [[ "$OSTYPE" == "darwin"* ]] && which leaks; then
+			MEMCHECK="leaks"
+	elif which valgrind; then
+		MEMCHECK="valgrind"
+	else  
+		echo "Could not find memcheck, please install valgrind!" 1>&2
+		exit 1
+	fi
+}
+
+find_memcheck
+
+"$MEMCHECK" "$NAME"\
+	&& echo -e "${GREEN}No leaks found!${NC}"\
+	|| echo -e "${RED}Leaks! ($?) ${NC}"
